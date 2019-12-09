3d5404ebb7479e1fc00983a03a70b40c42ecfa06
Milica-Sudar <milicasudar@gmail.com>
2019-12-09 17:43:05 +0100

    Dodata klasa tacka

---
+++ Point.cpp
@@ -0,0 +1,11 @@
+#include "Point.h"
+
+Point::Point()
+{
+ x=0; y = 0; z=0;
+}
+
+Point::Point(float x1, float y1, float z1)
+{
+ x = x1; y = y1; z=z1;
+}
---
+++ Point.h
@@ -0,0 +1,7 @@
+class Point
+{
+public:
+	float x, y, z;
+	Point();
+	Point(float x1, float y1, float z1);
+};
