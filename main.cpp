a0da923373e78de811890fd0931991ca87f49bc2
Milica-Sudar <milicasudar@gmail.com>
2020-04-17 13:22:57 +0200

    Dodata randomizacija Pyraminx-a

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include "Pyramidix.h"
 #include "image.h"
+#include <time.h>
 #define TIMER_INTERVAL 50
 #define TIMER_ID 0
-
-#define START_TIMER_ID (1)
-#define START_TIMER_INT (60)
+#define START_TIMER_ID 1
+#define START_TIMER_INT 60
 
 /* Ime fajla sa teksturom*/
 #define FILENAME0 "background.bmp"
@@ -32,12 +32,15 @@ char rotationFlag = 't';
 //Flag male ili velike rotacije s - mala, l - velika
 char sizeFlag = 's';
 
+/*Promenljiva za randomizaciju*/
+int randomizeCount = 20;
 
+/*Promenljive za timer funkcije*/
 int curentTick = 0;
 int MaxTick = 9;
 
 
-//promenljive za pocetnu animaciju
+/*promenljive za pocetnu animaciju*/
 double start_parameter=0;
 static int start_ongoing=1;
 
@@ -51,6 +54,10 @@ static void on_display(void);
 static void on_timer(int);
 static void rotate(int degree);
 static void start_timer(int value);
+static void randomize();
+static void on_randomizer_timer(int value);
+static int randNum(int min, int max);
+static void setDirection(int a, int b, int c);
 
 using namespace std;
 
@@ -60,6 +67,9 @@ int main(int argc,char** argv){
 
   PX = Pyramidix();
   animation_ongoing = 0;
+
+  /*Inicijalizacija random seed*/
+  srand(time(0));
 
   /* inicijalizuje se GLUT*/
   glutInit(&argc, argv);
@@ -125,7 +135,7 @@ int main(int argc,char** argv){
   glLoadIdentity();
   glGetFloatv(GL_MODELVIEW_MATRIX, matrix);
 
-
+  /*Pokrece se pocetna animacija*/
   if(start_ongoing)
         glutTimerFunc(START_TIMER_INT, start_timer, START_TIMER_ID);
 
@@ -312,7 +322,13 @@ static void on_keyboard(unsigned char key, int x, int y)
       glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
     }
 		break;
-
+  case 'p':
+	case 'P':
+		if (!animation_ongoing) {
+			randomizeCount = 20;
+			randomize();
+		}
+    break;
 	}
 }
 
@@ -363,12 +379,12 @@ static void on_display(void)
   glBindTexture(GL_TEXTURE_2D, 0);
 
 
-
+  /*Postavlja se pogled na piramidu*/
   glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
   gluLookAt(0, 0, 12-start_parameter*5, 0, 0, 0, 0, 1, 0);
 
-
+  /*Prekida se pocetna animacija*/
   if(start_parameter>=1)
     start_ongoing=0;
 
@@ -376,7 +392,10 @@ static void on_display(void)
  /*Matrica za mis*/
  glMultMatrixf(matrix);
 
+ /*Crtanje piraminx-a*/
 	PX.Draw();
+
+  /*Postavljanje kratke rotacije prilikom pocetne animacije*/
   if(start_parameter >=0.4 && start_parameter<=1)
     PX.RotationOnY+=1;
 
@@ -427,6 +446,7 @@ static void on_timer(int value)
     rotate(-12);
 
   curentTick++;
+  /*Ukoliko je curentTick veci od MaxTick prekida se animacija za rotaciju*/
   if (curentTick > MaxTick) {
     animation_ongoing = 0;
     curentTick = 0;
@@ -469,7 +489,7 @@ static void on_motion(int x, int y)
     glutPostRedisplay();
 }
 
-//tajmer za pocetnu animaciju
+/*tajmer za pocetnu animaciju*/
 static void start_timer(int value){
 
     if(value!=START_TIMER_ID){
@@ -479,8 +499,80 @@ static void start_timer(int value){
     start_parameter+=0.01;
 
 
+
     glutPostRedisplay();
     if(start_ongoing){
         glutTimerFunc(START_TIMER_INT, start_timer, START_TIMER_ID);
     }
+}
+/*tajmer za randomizaciju*/
+static void on_randomizer_timer(int value)
+{
+
+	if (value != TIMER_ID)
+		return;
+	if (directionFlag == 'l')
+		rotate(12);
+	else
+		rotate(-12);
+
+	curentTick++;
+	if (curentTick > MaxTick) {
+		animation_ongoing = 0;
+		curentTick = 0;
+		randomizeCount--;
+		randomize();
+		return;
+	}
+	glutPostRedisplay();
+	if (animation_ongoing) {
+		glutTimerFunc(TIMER_INTERVAL, on_randomizer_timer, TIMER_ID);
+	}
+}
+/*Vraca random broj iz intervala [min,max]*/
+static int randNum(int min, int max)
+{
+  int range = max - min + 1;
+  int num = rand() % range + min;
+  return num;
+}
+/*Postavlja parametre za rotaciju gde d predtsvlja directionFlag,sF sizeFlag i rF rotationFlag*/
+static void setDirection(int d, int sF, int rF) {
+	if (d == 0) {
+		directionFlag = 'l';
+	}
+	else {
+		directionFlag = 'r';
+	}
+	if (sF == 0) {
+		sizeFlag = 'l';
+	}
+	else {
+		sizeFlag = 's';
+	}
+	if (rF == 0) {
+		rotationFlag = 'b';
+	}
+	else if (rF == 1) {
+		rotationFlag = 'l';
+	}
+	else if (rF == 2) {
+		rotationFlag = 'r';
+	}
+	else {
+		rotationFlag = 't';
+	}
+}
+/*Funcija koja vrsi randomizaciju Pyraminx-a*/
+static void randomize() {
+	if (randomizeCount < 1)
+    return;
+	if (!animation_ongoing) {
+		int d = randNum(0, 1);
+		int sF = randNum(0, 1);
+		int rF = randNum(0, 3);
+		setDirection(d, sF, rF);
+		animation_ongoing = 1;
+		glutTimerFunc(TIMER_INTERVAL, on_randomizer_timer, TIMER_ID);
+	}
 }
